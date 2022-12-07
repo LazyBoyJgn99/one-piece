26ebfc90980bf290962a366556faef46d5da64b6
626723063 <626723063@qq.com>
2022-12-07 17:59:42 +0800

    feat: init

---
+++ src/components/action-btn/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './action-btn.css';
+function ActionBtn(props: any) {
+  return (
+    <div className="actionBtn" {...props}>
+      ActionBtn
+    </div>
+  );
+}
+
+export default ActionBtn;
